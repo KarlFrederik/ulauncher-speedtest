b197e963b58c1653e39c32201bf828b283010873
Karl-Frederik Schlüter <karl.frederik@freelinesystem.de>
2021-06-28 18:23:43 +0200

    import pynotifier & speedtest-cli, init notifier and customized KeywordQueryEventListener

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from ulauncher.api.client.Extension import Extension
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from pynotifier import Notification
+import speedtest
+
+s = speedtest.Speedtest()
+
+supload = s.upload()
+sdownload = s.download()
+
+notifiyer =    Notification(
+	        title='Speedtest Results',
+	        description=supload,
+	        icon_path='images/icon.png',
+            duration=10,
+            urgency='normal'
+		).send()
+
 
 
 class DemoExtension(Extension):
@@ -21,7 +40,7 @@ class KeywordQueryEventListener(EventListener):
             items.append(ExtensionResultItem(icon='images/icon.png',
                                              name='Item %s' % i,
                                              description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+                                             on_enter=notifiyer))
 
         return RenderResultListAction(items)
 
