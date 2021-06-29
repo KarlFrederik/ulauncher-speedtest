bcb0071780d45d50a63823bab4a0164ce2d8c13f
Karl-Frederik Schlüter <karl.frederik@freelinesystem.de>
2021-06-29 22:39:34 +0200

    created a installer for installing PIP, py-notifier and speedtest-cli

---
+++ installscript.sh
@@ -0,0 +1,8 @@
+# ulauncher-speedtest init description
+# by Karl_F
+
+#This Script will Install PIP, speedtest-cli and pynotifier
+
+sudo apt install python3-pip
+pip install py-notifier
+pip install speedtest-cli
