15cf0cefed47421d3fcbf28dc4df014bb3b2acae
Karl-Frederik Schlüter <karl.frederik@freelinesystem.de>
2021-07-01 16:33:58 +0200

    deleted useless code to optimise the speed

--- main.py
+++ main.py
@@ -20,38 +20,6 @@ from datetime import datetime
 # Initalised speedtest.net
 s = speedtest.Speedtest()
 
-# Sends a Push-Notification while the Speedtest is Preparing
-notifiyerRunning =    Notification(
-	        title='Speedtest Running',
-	        description='Please wait while the Speedtest is running',
-	        icon_path='.local/share/ulauncher/extensions/speedtest.test/images/icon.png',
-            duration=20,
-            urgency='normal'
-		).send()
-
-
-
-# Does Speedtest and stores the result into upList for Upload, downList for Download and pingList for Ping
-upList = [0];
-upList = round((round(s.upload()) / 1048576), 2)
-downList = [0]
-downList = round((round(s.download()) / 1048576), 2)
-pingList = [0]
-pingList = round(s.results.ping)
-
-
-
-# Sends a Notification with the Speedtest result
-notifiyer =    Notification(
-	        title='Speedtest Results',
-	        description='Download: ' + str(downList) + " mbps" + ", Upload: " + str(upList) + " mbps, " + 'Ping: ' + str(pingList) + ' ms',
-	        icon_path='images/icon.png',
-            duration=30,
-            urgency='normal'
-		).send()
-
-notifyOnEnter = notifiyerRunning, notifiyer
-
 
 class DemoExtension(Extension):
 
